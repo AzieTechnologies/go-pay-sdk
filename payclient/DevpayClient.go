07e0097082cef7972dcfcb7e058673212b45aed1
jnixdev <jnixdev@gmail.com>
2021-07-14 10:42:11 +0530

    Renamed client name

--- payclient/DevpayClient.go
+++ payclient/DevpayClient.go
@@ -23,7 +23,7 @@ const (
 	XCHF = "chf"
 )
 
-type TilledClient struct {
+type DevpayClient struct {
 	Config *Config
 }
 
@@ -45,28 +45,28 @@ var (
 	InfoLogger *log.Logger
 )
 
-func New(config *Config) *TilledClient {
+func New(config *Config) *DevpayClient {
 	if config == nil {
 		config = &Config{}
 		config.Sandbox = false
 		config.EnableVerboseLogging = false
 	}
 
-	client := &TilledClient{
+	client := &DevpayClient{
 		Config: config,
 	}
 	return client
 }
 
-func (tilledClient *TilledClient) ConfirmPayment(paymntDetail PaymentDetail) (paymentResponse *PaymentIntent, err error) {
+func (devpayClient *DevpayClient) ConfirmPayment(paymntDetail PaymentDetail) (paymentResponse *PaymentIntent, err error) {
 
 	var InfoLogger *log.Logger = nil
 	// Create InfoLogger if verbose logging is enabled
-	if tilledClient.Config.EnableVerboseLogging {
+	if devpayClient.Config.EnableVerboseLogging {
 		InfoLogger = log.New(os.Stdout, "INFO ", log.Ldate|log.Ltime|log.Lshortfile)
 	}
 
-	restClient := tilledClient.createRestClient()
+	restClient := devpayClient.createRestClient()
 	restClient.InfoLogger = InfoLogger
 
 	// Get Paysafe API key
@@ -78,7 +78,7 @@ func (tilledClient *TilledClient) ConfirmPayment(paymntDetail PaymentDetail) (pa
 	}
 
 	// Get payment token
-	paysafe := &Paysafe{Sandbox: tilledClient.Config.Sandbox,
+	paysafe := &Paysafe{Sandbox: devpayClient.Config.Sandbox,
 		APIKey:     paysafeAPIKey,
 		InfoLogger: InfoLogger}
 
@@ -89,8 +89,8 @@ func (tilledClient *TilledClient) ConfirmPayment(paymntDetail PaymentDetail) (pa
 
 	// Create payment manager
 	paymentManager := &PaymentManager{RestAPIClient: restClient,
-		Config:              tilledClient.Config,
-		PaymentIntentSecret: tilledClient.Config.Secret}
+		Config:              devpayClient.Config,
+		PaymentIntentSecret: devpayClient.Config.Secret}
 
 	bd := BillingDetail{BillingAddress: paymntDetail.BillingAddress,
 		Name:  paymntDetail.Name,
@@ -108,15 +108,15 @@ func (tilledClient *TilledClient) ConfirmPayment(paymntDetail PaymentDetail) (pa
 	return paymentManager.ConfirmPayment(payMethodDetail)
 }
 
-func (tilledClient *TilledClient) createRestClient() *RestAPIClient {
+func (devpayClient *DevpayClient) createRestClient() *RestAPIClient {
 	var header = make(map[string]string)
-	header["Authorization"] = "Bearer " + tilledClient.Config.ShareableKey
-	header["Tilled-Account"] = tilledClient.Config.AccountId
+	header["Authorization"] = "Bearer " + devpayClient.Config.ShareableKey
+	header["Tilled-Account"] = devpayClient.Config.AccountId
 	header["Content-Type"] = "application/json"
 
 	// Select end point
 	var endPoint = TilledEndPoint
-	if tilledClient.Config.Sandbox {
+	if devpayClient.Config.Sandbox {
 		endPoint = TilledSanboxEndPoint
 	}
 
